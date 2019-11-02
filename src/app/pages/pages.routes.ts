7238f93ce7269efed9f1289b06a3f540b95bb441
betolix <betolix@gmail.com>
2019-11-02 12:25:13 -0500

    Fin Sección 17

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -10,11 +10,14 @@ import { RxjsComponent } from './rxjs/rxjs.component';
 
 import { ProfileComponent } from './profile/profile.component';
 
-import { LoginGuardGuard } from '../services/service.index';
 import { UsuariosComponent } from './usuarios/usuarios.component';
 import { HospitalesComponent } from './hospitales/hospitales.component';
 import { MedicosComponent } from './medicos/medicos.component';
 import { MedicoComponent } from './medicos/medico.component';
+import { BusquedaComponent } from './busqueda/busqueda.component';
+
+// GUARDS
+import { LoginGuardGuard, AdminGuard } from '../services/service.index';
 
 
 
@@ -31,8 +34,16 @@ const pagesRoutes: Routes = [
           { path: 'rxjs', component: RxjsComponent, data: {titulo: 'RxJs'}  },
           { path: 'account-settings', component: AccountSettingsComponent, data: {titulo: 'Ajustes del Tema'}  },
           { path: 'perfil', component: ProfileComponent, data: {titulo: 'Perfil de usuario'}  },
+          { path: 'busqueda/:termino', component: BusquedaComponent, data: {titulo: 'Buscador'}  },
           // MANTENIMIENTOS
-          { path: 'usuarios', component: UsuariosComponent, data: {titulo: 'Mantenimiento de Usuarios'}  },
+          
+          { 
+            path: 'usuarios', 
+            component: UsuariosComponent, 
+            canActivate: [ AdminGuard ],
+            data: {titulo: 'Mantenimiento de Usuarios'}  
+          },
+          
           { path: 'hospitales', component: HospitalesComponent, data: {titulo: 'Mantenimiento de Hospitales'}  },
           { path: 'medicos', component: MedicosComponent, data: {titulo: 'Mantenimiento de Médicos'}  },
           { path: 'medico/:id', component: MedicoComponent, data: {titulo: 'Actualizar Médico'}  },
--- src/app/services/usuario/usuario.service.ts
+++ src/app/services/usuario/usuario.service.ts
@@ -6,9 +6,12 @@ import { URL_SERVICIOS } from 'src/app/config/config';
 
 // import 'rxjs/add/operator/map';
 // import { map } from 'rxjs/operators';
-import { map } from 'rxjs/operators'
+import { map } from 'rxjs/operators';
+import { catchError } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
-import Swal from 'sweetalert2'
+
+import Swal from 'sweetalert2';
 import { Router } from '@angular/router';
 import { SubirArchivoService } from '../subir-archivo/subir-archivo.service';
 import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
@@ -21,6 +24,7 @@ export class UsuarioService {
 
   usuario: Usuario;
   token: string;
+  menu: any[] = [];
 
   constructor(
     public http: HttpClient,
@@ -41,31 +45,37 @@ export class UsuarioService {
     if ( localStorage.getItem('token') ){
       this.token = localStorage.getItem('token');
       this.usuario = JSON.parse ( localStorage.getItem('usuario') );
+      this.menu = JSON.parse ( localStorage.getItem('menu') );
     }else {
       this.token = '';
       this.usuario = null;
+      this.menu = [];
     }
   }
 
 
 
-  guardarStorage( id: string, token: string, usuario: Usuario ) {
+  guardarStorage( id: string, token: string, usuario: Usuario, menu: any ) {
 
     localStorage.setItem('id', id );
     localStorage.setItem('token', token );
     localStorage.setItem('usuario', JSON.stringify( usuario ) );
+    localStorage.setItem('menu', JSON.stringify( menu ) );
 
     this.usuario = usuario;
     this.token = token;
-
+    this.menu = menu;
+    
   }
 
 logout(){
   this.usuario = null;
   this.token = '';
+  this.menu = [];
 
   localStorage.removeItem('token');
   localStorage.removeItem('usuario');
+  localStorage.removeItem('menu');
 
   this.router.navigate(['/login']);
 
@@ -77,7 +87,8 @@ logout(){
 
     return this.http.post( url, { token: token } )
         .pipe( map( ( resp: any ) => {
-          this.guardarStorage( resp.id, resp.token, resp.usuario );
+          this.guardarStorage( resp.id, resp.token, resp.usuario, resp.menu );
+          // console.log( resp );
           return true;
         } ) );
     
@@ -96,16 +107,26 @@ logout(){
     let url = URL_SERVICIOS + '/login';
     return this.http.post( url, usuario )
         .pipe(map( (resp: any ) => {
-          
-          this.guardarStorage( resp.id, resp.token, resp.usuario );
-          
+          this.guardarStorage( resp.id, resp.token, resp.usuario, resp.menu );
           // localStorage.setItem('id', resp.id );
           // localStorage.setItem('token', resp.token );
           // localStorage.setItem('usuario', JSON.stringify( resp.usuario ) );
-
           return true;
+        }), catchError( err => {
 
-        } ))
+          // console.log( err.error.mensaje );
+          Swal.fire({
+            title: 'Error en el login!',
+            text: err.error.mensaje,
+            type: 'error',
+            confirmButtonText: 'Ok'
+          })
+          return Observable.throw ( err );
+          
+
+        } )
+        
+        );
   }
 
 
@@ -126,7 +147,20 @@ logout(){
           confirmButtonText: 'Ok'
         })
         return resp.usuario;
-    }));
+    }), catchError( err => {
+
+      // console.log( err.error.mensaje );
+      Swal.fire({
+        title: err.error.mensaje,
+        text: err.error.errors.message,
+        type: 'error',
+        confirmButtonText: 'Ok'
+      })
+      return Observable.throw ( err );
+      
+    } )
+    
+    );
 
   }
 
@@ -144,11 +178,8 @@ logout(){
           
           if ( usuario._id === this.usuario._id ) {
             let usuarioDB: Usuario = resp.usuario;
-            this.guardarStorage( usuarioDB._id, this.token, usuarioDB  );
-          }
-          
-
-
+            this.guardarStorage( usuarioDB._id, this.token, usuarioDB, this.menu );
+          }         
           Swal.fire({
             title: 'Usuario actualizado!',
             text: usuario.nombre,
@@ -157,8 +188,20 @@ logout(){
           })
 
           return true;
+        }), catchError( err => {
+
+          // console.log( err.error.mensaje );
+          Swal.fire({
+            title: err.error.mensaje,
+            text: err.error.errors.message,
+            type: 'error',
+            confirmButtonText: 'Ok'
+          })
+          return Observable.throw ( err );
           
-        } ))
+        } )
+        
+        );
 
   }
 
@@ -178,7 +221,7 @@ logout(){
           })
 
           //ACTUALIZAR STORAGE
-          this.guardarStorage( id, this.token, this.usuario );
+          this.guardarStorage( id, this.token, this.usuario, this.menu );
 
 
         } )
